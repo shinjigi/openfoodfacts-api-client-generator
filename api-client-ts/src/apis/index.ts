d6705e581854a96a8f530fc603de2d9c835eeb10
Luigi de Pinto <shinjigi@gmail.com>
2024-07-01 10:21:31 +0200

    first all

---
+++ api-client-ts/src/apis/index.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './DefaultApi';
+export * from './PersonalSearchApi';
+export * from './ReadRequestsApi';
+export * from './WriteRequestsApi';
